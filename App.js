dc22b964ca5fa7cd43c8791e575f4b15e92b51b0
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2025-03-07 16:41:57 +0530

    3. Laying the foundation

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+// 1. Print "Namaste React" by using React
+// const heading = React.createElement(
+//     "h1", 
+//     {id: "heading"}, 
+//     "Namaste React");
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading)
+
+// 2. Print "Namaster React by using JSX using" JSX
+// const jsxHeading = (
+//         <h1 id="heading">
+//         Namaste React by using JSX 
+//         </h1>
+//     );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(jsxHeading);
+
+// Functional Components: 
+// const HeadingComponent = () => {
+//     return <h1 className="heading"> Namaste React Functional Components </h1>
+// }
+
+// root = ReactDOM.createRoot(document.getElementById("root"))
+
+// root.render(<HeadingComponent/>)
 
 
-const heading = React.createElement(
-    "div",
-    { id: "parent" }, 
 
-    React.createElement("div",
-        {id: "child"},[
-        React.createElement("h1", {}, "I'm h1 tag"),
-        React.createElement("h2", {}, "I'm h2 tag")
-    ]
-    )
-);
+//Create jsx
+// const jsxHeading = <h1 className="heading">Namaste React by using jsx</h1>
 
-console.log(heading);  // print object.
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(jsxHeading)
 
-root.render(heading);
+// Create Functional Components
+
+const number = 1000;
+
+const HeadingComponent = () => (
+    <div id="container">
+        {number}
+        <h1> Namaste React by using Functional Component</h1>
+
+    </div>
+    
+)
+
+root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent/>)
+
+
+
+
+
+
